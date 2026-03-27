d5733914864f3b54254bf58a5934594bfcc06c14
Thomas Rawson <thomas.rawson@demography.ox.ac.uk>
2026-03-27 16:36:28 +0000

    Add Figure 2 task

---
+++ src/Fig2_Health_Outcomes/plotting_functions.R
@@ -0,0 +1,367 @@
+### All Fig2 plotting functions
+######################################
+#Panel 1
+plot_time_series <- function(sim_df, data_df, outcome_label, region_selected) {
+  #Key dates here:
+  grey_lines <- c(
+    "2020-03-25", ## First full lockdown
+    "2020-05-11", ## Initial easing
+    "2020-11-05", ## Lockdown 2 starts
+    "2020-12-02", ## Lockdown 2 ends
+    "2021-01-05", ## 16. Lockdown 3 starts
+    "2021-03-08", ## 17. Step 1 of roadmap: schools reopen
+    "2021-04-19", ## 19. Step 2 of roadmap: outdoors hospitality (04-12) 
+    ##     and schools return (04-19)
+    "2021-05-17", ## 20. Step 3 of roadmap: indoors hospitality
+    "2021-07-19") ## 24. Step 4
+  grey_labels <- as.Date(grey_lines)
+  label_cols <- c(
+    "red",
+    "grey",
+    "red",
+    "grey",
+    "red",
+    "grey",
+    "grey",
+    "grey",
+    "grey"
+  )
+  #We give some label positions a nudge to aid plotting
+  grey_labels[5] <- grey_labels[5] 
+  grey_labels[4] <- grey_labels[4] + 10
+  
+  ggplot(filter(sim_df, region == region_selected), 
+         aes(x = sircovid::sircovid_date_as_date(time), y = mean, colour = version, fill = version)) +
+    geom_ribbon(aes(ymin = cri_lower, ymax = cri_upper), alpha = 0.2, colour = NA) +
+    geom_line(size = 1) +
+    geom_point(data = filter(data_df, region == region_selected), 
+               aes(x=as.Date(time), y = value, color = "Data", fill = "Data"), alpha = 0.3) +
+    geom_vline(xintercept = as.Date('2021-03-31'), alpha = 0.8, color = 'black') +
+    geom_vline(xintercept = as.Date(grey_lines), alpha = 0.7, color = label_cols, lty = 'dashed') +
+    scale_colour_manual(values = c("Factual" = "#1b9e77",
+                                   "Counterfactual_principal" = "#d95f02",
+                                   "Counterfactual_low_migration" = "#0099B4FF",
+                                   "Counterfactual_high_migration" = "#925E9FFF",
+                                   "Data" = "#AD002AFF"),
+                        breaks = c("Factual", "Counterfactual_principal",
+                                   "Counterfactual_low_migration",
+                                   "Counterfactual_high_migration", "Data"),
+                        labels = c("2019 baseline", "2047 central \nprojection", 
+                                   "2047 low migration \nprojection",
+                                   "2047 high migration \nprojection", "Data")) +
+    scale_fill_manual(values = c("Factual" = "#1b9e77",
+                                 "Counterfactual_principal" = "#d95f02",
+                                 "Counterfactual_low_migration" = "#0099B4FF",
+                                 "Counterfactual_high_migration" = "#925E9FFF",
+                                 "Data" = "#AD002AFF"),
+                      breaks = c("Factual", "Counterfactual_principal",
+                                 "Counterfactual_low_migration",
+                                 "Counterfactual_high_migration", "Data"),
+                      labels = c("2019 baseline", "2047 central \nprojection", 
+                                 "2047 low migration \nprojection",
+                                 "2047 high migration \nprojection", "Data")) +
+    labs(
+      x = "Time",
+      y = outcome_label,
+      colour = "Scenario:",
+      fill = "Scenario:",
+      title = sprintf("%s - %s",outcome_label, region_selected)
+    ) +
+    theme_classic(base_size = 14) +
+    theme(
+      legend.position = "top",
+      panel.grid.minor = element_blank()
+    ) +
+    scale_x_date(date_labels = "%b %Y")
+}
+
+plot_time_series_per_capita <- function(sim_df, data_df, outcome_label, region_selected) {
+  #Key dates here:
+  grey_lines <- c(
+    "2020-03-25", ## First full lockdown
+    "2020-05-11", ## Initial easing
+    "2020-11-05", ## Lockdown 2 starts
+    "2020-12-02", ## Lockdown 2 ends
+    "2021-01-05", ## 16. Lockdown 3 starts
+    "2021-03-08", ## 17. Step 1 of roadmap: schools reopen
+    "2021-04-19", ## 19. Step 2 of roadmap: outdoors hospitality (04-12) 
+    ##     and schools return (04-19)
+    "2021-05-17", ## 20. Step 3 of roadmap: indoors hospitality
+    "2021-07-19") ## 24. Step 4
+  grey_labels <- as.Date(grey_lines)
+  label_cols <- c(
+    "red",
+    "grey",
+    "red",
+    "grey",
+    "red",
+    "grey",
+    "grey",
+    "grey",
+    "grey"
+  )
+  #We give some label positions a nudge to aid plotting
+  grey_labels[5] <- grey_labels[5] 
+  grey_labels[4] <- grey_labels[4] + 10
+  
+  ggplot(filter(sim_df, region == region_selected), 
+         aes(x = sircovid::sircovid_date_as_date(time), y = (mean/population)*1000, colour = version, fill = version)) +
+    geom_ribbon(aes(ymin = (cri_lower/population)*1000, ymax = (cri_upper/population)*1000), alpha = 0.2, colour = NA) +
+    geom_line(size = 1) +
+    geom_point(data = filter(data_df, region == region_selected), 
+               aes(x=as.Date(time), y = (value/population)*1000, color = "Data", fill = "Data"), alpha = 0.3) +
+    geom_vline(xintercept = as.Date('2021-03-31'), alpha = 0.8, color = 'black') +
+    geom_vline(xintercept = as.Date(grey_lines), alpha = 0.7, color = label_cols, lty = 'dashed') +
+    scale_colour_manual(values = c("Factual" = "#1b9e77",
+                                   "Counterfactual_principal" = "#d95f02",
+                                   "Counterfactual_low_migration" = "#0099B4FF",
+                                   "Counterfactual_high_migration" = "#925E9FFF",
+                                   "Data" = "#AD002AFF"),
+                        breaks = c("Factual", "Counterfactual_principal",
+                                   "Counterfactual_low_migration",
+                                   "Counterfactual_high_migration", "Data"),
+                        labels = c("2019 baseline", "2047 central \nprojection", 
+                                   "2047 low migration \nprojection",
+                                   "2047 high migration \nprojection", "Data")) +
+    scale_fill_manual(values = c("Factual" = "#1b9e77",
+                                 "Counterfactual_principal" = "#d95f02",
+                                 "Counterfactual_low_migration" = "#0099B4FF",
+                                 "Counterfactual_high_migration" = "#925E9FFF",
+                                 "Data" = "#AD002AFF"),
+                      breaks = c("Factual", "Counterfactual_principal",
+                                 "Counterfactual_low_migration",
+                                 "Counterfactual_high_migration", "Data"),
+                      labels = c("2019 baseline", "2047 central \nprojection", 
+                                 "2047 low migration \nprojection",
+                                 "2047 high migration \nprojection", "Data")) +
+    labs(
+      x = "Time",
+      y = sprintf("%s (per 1000 people)", outcome_label),
+      colour = "Scenario:",
+      fill = "Scenario:",
+      title = sprintf("%s - %s", outcome_label, region_selected)
+    ) +
+    theme_classic(base_size = 14) +
+    theme(
+      legend.position = "top",
+      panel.grid.minor = element_blank()
+    ) +
+    scale_x_date(date_labels = "%b %Y")
+}
+
+
+#######
+# Panel 2, cumulative
+plot_cumulative <- function(sim_df, outcome_label, region_selected, y_nudge_value = -50) {
+  
+  plot_df <- sim_df |>
+    dplyr::filter(region == region_selected) |>
+    dplyr::group_by(version) |>
+    dplyr::arrange(time, .by_group = TRUE) |>
+    dplyr::mutate(cum_mean = cumsum(mean)) |>
+    dplyr::ungroup()
+  
+  # Order versions so the largest cumulative curve is drawn first (back),
+  # and the smallest is drawn last (front)
+  version_order <- plot_df |>
+    dplyr::group_by(version) |>
+    dplyr::summarise(final_cum = max(cum_mean), .groups = "drop") |>
+    dplyr::arrange(dplyr::desc(final_cum)) |>
+    dplyr::pull(version)
+  
+  plot_df$version <- factor(plot_df$version, levels = version_order)
+  
+  label_df <- plot_df |>
+    dplyr::group_by(version) |>
+    dplyr::filter(time == max(time)) |>
+    dplyr::ungroup()
+  
+  ggplot(
+    plot_df,
+    aes(
+      x = sircovid::sircovid_date_as_date(time),
+      y = cum_mean/1000,
+      fill = version,
+      colour = version,
+      group = version
+    )
+  ) +
+    geom_area(position = "identity") +
+    geom_label(
+      data = label_df,
+      aes(
+        x = sircovid::sircovid_date_as_date(time),
+        y = cum_mean/1000,
+        label = round(cum_mean, 0),
+        colour = version
+      ),
+      fill = "white",
+      size = 3,
+      #label.size = NA,   # removes border
+      show.legend = FALSE,
+      hjust = -0.1,
+      nudge_y = y_nudge_value,
+      nudge_x = -50
+    ) +
+    #geom_line(linewidth = 1) +
+    scale_colour_manual(
+      values = c(
+        "Factual" = "#1b9e77",
+        "Counterfactual_principal" = "#d95f02",
+        "Counterfactual_low_migration" = "#0099B4FF",
+        "Counterfactual_high_migration" = "#925E9FFF"
+      ),
+      breaks = c(
+        "Factual",
+        "Counterfactual_principal",
+        "Counterfactual_low_migration",
+        "Counterfactual_high_migration"
+      ),
+      labels = c(
+        "2019 baseline",
+        "2047 central \nprojection",
+        "2047 low migration \nprojection",
+        "2047 high migration \nprojection"
+      )
+    ) +
+    scale_fill_manual(
+      values = c(
+        "Factual" = "#1b9e77",
+        "Counterfactual_principal" = "#d95f02",
+        "Counterfactual_low_migration" = "#0099B4FF",
+        "Counterfactual_high_migration" = "#925E9FFF"
+      ),
+      breaks = c(
+        "Factual",
+        "Counterfactual_principal",
+        "Counterfactual_low_migration",
+        "Counterfactual_high_migration"
+      ),
+      labels = c(
+        "2019 baseline",
+        "2047 central \nprojection",
+        "2047 low migration \nprojection",
+        "2047 high migration \nprojection"
+      )
+    ) +
+    labs(
+      x = "Time",
+      y = paste0("Cumulative ", outcome_label),
+      colour = "Scenario:",
+      fill = "Scenario:",
+      title = sprintf("%s - %s", outcome_label, region_selected)
+    ) +
+    theme_classic(base_size = 14) +
+    theme(
+      legend.position = "top",
+      panel.grid.minor = element_blank()
+    ) +
+    scale_x_date(date_labels = "%b %Y")
+}
+
+plot_cumulative_per_capita <- function(sim_df, outcome_label, region_selected, y_nudge_value = 0) {
+  
+  plot_df <- sim_df |>
+    dplyr::filter(region == region_selected) |>
+    dplyr::group_by(version) |>
+    dplyr::arrange(time, .by_group = TRUE) |>
+    dplyr::mutate(cum_mean = cumsum(mean)) |>
+    dplyr::ungroup()
+  
+  # Order versions so the largest cumulative curve is drawn first (back),
+  # and the smallest is drawn last (front)
+  version_order <- plot_df |>
+    dplyr::group_by(version) |>
+    dplyr::summarise(final_cum = max(cum_mean), .groups = "drop") |>
+    dplyr::arrange(dplyr::desc(final_cum)) |>
+    dplyr::pull(version)
+  
+  plot_df$version <- factor(plot_df$version, levels = version_order)
+  
+  label_df <- plot_df |>
+    dplyr::group_by(version) |>
+    dplyr::filter(time == max(time)) |>
+    dplyr::ungroup()
+  
+  ggplot(
+    plot_df,
+    aes(
+      x = sircovid::sircovid_date_as_date(time),
+      y = cum_mean/population,
+      fill = version,
+      colour = version,
+      group = version
+    )
+  ) +
+    geom_area(position = "identity") +
+    geom_label(
+      data = label_df,
+      aes(
+        x = sircovid::sircovid_date_as_date(time),
+        y = cum_mean/population,
+        label = round(cum_mean, 0),
+        colour = version
+      ),
+      fill = "white",
+      size = 3,
+      #label.size = NA,   # removes border
+      show.legend = FALSE,
+      hjust = -0.1,
+      nudge_y = y_nudge_value,
+      nudge_x = -50
+    ) +
+    #geom_line(linewidth = 1) +
+    scale_colour_manual(
+      values = c(
+        "Factual" = "#1b9e77",
+        "Counterfactual_principal" = "#d95f02",
+        "Counterfactual_low_migration" = "#0099B4FF",
+        "Counterfactual_high_migration" = "#925E9FFF"
+      ),
+      breaks = c(
+        "Factual",
+        "Counterfactual_principal",
+        "Counterfactual_low_migration",
+        "Counterfactual_high_migration"
+      ),
+      labels = c(
+        "2019 baseline",
+        "2047 central \nprojection",
+        "2047 low migration \nprojection",
+        "2047 high migration \nprojection"
+      )
+    ) +
+    scale_fill_manual(
+      values = c(
+        "Factual" = "#1b9e77",
+        "Counterfactual_principal" = "#d95f02",
+        "Counterfactual_low_migration" = "#0099B4FF",
+        "Counterfactual_high_migration" = "#925E9FFF"
+      ),
+      breaks = c(
+        "Factual",
+        "Counterfactual_principal",
+        "Counterfactual_low_migration",
+        "Counterfactual_high_migration"
+      ),
+      labels = c(
+        "2019 baseline",
+        "2047 central \nprojection",
+        "2047 low migration \nprojection",
+        "2047 high migration \nprojection"
+      )
+    ) +
+    labs(
+      x = "Time",
+      y = paste0("Cumulative ", outcome_label, " (per capita)"),
+      colour = "Scenario:",
+      fill = "Scenario:",
+      title = sprintf("%s - %s", outcome_label, region_selected)
+    ) +
+    theme_classic(base_size = 14) +
+    theme(
+      legend.position = "top",
+      panel.grid.minor = element_blank()
+    ) +
+    scale_x_date(date_labels = "%b %Y")
+}
---
+++ src/Fig2_Health_Outcomes/script.R
@@ -0,0 +1,302 @@
+
+source("global_util.R")
+## Load simulation data
+########################
+#Base params
+principal_base_params <- readRDS("dependencies/baseline_central.rds")
+principal_base_params$england <- list(
+  population = Reduce(`+`, lapply(principal_base_params, `[[`, "population"))
+)
+low_migration_base_params <- readRDS("dependencies/baseline_low_migration.rds")
+low_migration_base_params$england <- list(
+  population = Reduce(`+`, lapply(low_migration_base_params, `[[`, "population"))
+)
+high_migration_base_params <- readRDS("dependencies/baseline_high_migration.rds")
+high_migration_base_params$england <- list(
+  population = Reduce(`+`, lapply(high_migration_base_params, `[[`, "population"))
+)
+
+# Simulation output and index names
+baseline_df <- readRDS("dependencies/baseline_combined_output_dataframe.rds")
+baseline_index_names <- readRDS("dependencies/baseline_index_names.rds")
+counterfactual_principal_df <- readRDS("dependencies/principal_output_dataframe.rds")
+counterfactual_principal_index_names <- readRDS("dependencies/principal_index_names.rds")
+counterfactual_low_migration_df <- readRDS("dependencies/low_migration_output_dataframe.rds")
+counterfactual_low_migration_index_names <- readRDS("dependencies/low_migration_index_names.rds")
+counterfactual_high_migration_df <- readRDS("dependencies/high_migration_output_dataframe.rds")
+counterfactual_high_migration_index_names <- readRDS("dependencies/high_migration_index_names.rds")
+
+#Check all the index names align
+if (!(identical(baseline_index_names, counterfactual_principal_index_names) && identical(baseline_index_names, counterfactual_low_migration_index_names) && identical(baseline_index_names, counterfactual_high_migration_index_names))) {
+  stop("Index names are not identical")
+}
+
+#Add population to the simulation output dataframes
+baseline_df$version <- "Factual"
+baseline_df <- baseline_df %>%
+  rowwise() %>%
+  mutate(population = sum(sircovid:::sircovid_population(region))) %>%
+  ungroup()
+
+counterfactual_principal_df$version <- "Counterfactual_principal"
+counterfactual_principal_df <- counterfactual_principal_df %>%
+  rowwise() %>%
+  mutate(population = sum(principal_base_params[[region]]$population)) %>%
+  ungroup()
+
+counterfactual_low_migration_df$version <- "Counterfactual_low_migration"
+counterfactual_low_migration_df <- counterfactual_low_migration_df %>%
+  rowwise() %>%
+  mutate(population = sum(low_migration_base_params[[region]]$population)) %>%
+  ungroup()
+
+counterfactual_high_migration_df$version <- "Counterfactual_high_migration"
+counterfactual_high_migration_df <- counterfactual_high_migration_df %>%
+  rowwise() %>%
+  mutate(population = sum(high_migration_base_params[[region]]$population)) %>%
+  ungroup()
+
+# Stick all simulation data together:
+simulation_data <- rbind(baseline_df, counterfactual_principal_df, 
+                         counterfactual_low_migration_df, counterfactual_high_migration_df)
+
+
+# Load the real-world data
+england_data <- read_csv("england_region_data.csv")
+# Sum them together to make the england totals:
+england_totals <- england_data %>%
+  group_by(date) %>%
+  summarise(across(where(is.numeric), sum, na.rm = TRUE))
+england_totals$region <- "england"
+england_data <- bind_rows(england_data, england_totals)
+england_data$Nothing <- NA
+england_data <- england_data %>%
+  rowwise() %>%
+  mutate(population = sum(sircovid:::sircovid_population(region))) %>%
+  ungroup()
+# NOTE: The cols named below won't appear in england totals because we can't share the data, they're all NAs
+# It's all the pillar 2 data
+colnames(england_data)[which(!(colnames(england_data) %in% colnames(england_totals)))]
+
+#Add together the age_specific hospitalisations so I can focus on totals:
+england_data <- england_data %>%
+  mutate(total_confirmed_admissions = rowSums(across(c("admissions_0_9",                         
+                                       "admissions_10_19",  "admissions_20_29",                       
+                                       "admissions_30_39",  "admissions_40_49",                       
+                                       "admissions_50_59",  "admissions_60_69",                       
+                                       "admissions_70_79",  "admissions_80_plus")),
+                              na.rm = TRUE))
+
+#Remove all the stuff we don't need anymore:
+rm(baseline_df, counterfactual_principal_df,
+   counterfactual_high_migration_df, counterfactual_low_migration_df,
+   england_totals,
+   principal_base_params, high_migration_base_params, low_migration_base_params,
+   counterfactual_principal_index_names, counterfactual_low_migration_index_names, counterfactual_high_migration_index_names)
+
+#########################################################################
+#Define general plotting functions
+# Lancet colours for reference:
+#"#00468BFF" "#ED0000FF" "#42B540FF" "#0099B4FF" "#925E9FFF" "#FDAF91FF" "#AD002AFF" "#ADB6B6FF"
+source("plotting_functions.R")
+####################################################################
+# Prep folders
+dir.create("Figures")
+dir.create("Figures/individual_panels")
+####################################################################
+#Plot 1 - confirmed hospital admissions
+p1_sim_data <- simulation_data %>%
+  filter(output_type == "total_hospitalisations") %>%
+  filter(version %in% c("Factual", "Counterfactual_principal"))
+p1_real_data <- england_data %>%
+  select(date, region, population, total_confirmed_admissions)
+colnames(p1_real_data) <- c("time", "region", "population", "value")
+
+p1 <- plot_time_series(p1_sim_data, p1_real_data,
+                       "Daily Hospitalisations", "england")
+p1 <- p1 + ggtitle("Daily Confirmed Hospitalisations")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p1.png",
+  plot = p1,
+  width = 10,      # inches
+  height = 6,      # inches
+  dpi = 320        # high resolution
+)
+
+p1_per_capita <- plot_time_series_per_capita(p1_sim_data, p1_real_data,
+                       "Daily Hospitalisations", "england")
+p1_per_capita <- p1_per_capita + ggtitle("Daily new confirmed hospitalisations per capita - England")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p1_per_capita.png",
+  plot = p1_per_capita,
+  width = 10,      # inches
+  height = 6,      # inches
+  dpi = 320        # high resolution
+)
+
+#Do for each separate region
+dir.create("Figures/individual_panels/by_region")
+regions <- sircovid:::regions("england")
+for(r in regions){
+  p <- plot_time_series(p1_sim_data, p1_real_data,
+                        "Daily Hospitalisations", r)
+  ggsave(
+    filename = sprintf("Figures/individual_panels/by_region/Fig2_p1_%s.png", r),
+    plot = p,
+    width = 10,      # inches
+    height = 6,      # inches
+    dpi = 320        # high resolution
+  )
+}
+
+############################################################################
+#Panel 2 - Cumulative hospitalisations by scenario
+p2_sim_data <- simulation_data %>%
+  filter(output_type == "total_hospitalisations")
+p2 <- plot_cumulative(p2_sim_data, "Hospitalisations \n(thousands)", "england")
+p2 <- p2 + ggtitle("Cumulative Daily Confirmed Hospitalisations")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p2.png",
+  plot = p2,
+  width = 10, height = 6, dpi = 320        
+)
+
+p2_per_capita <- plot_cumulative_per_capita(p2_sim_data, "Hospitalisations", "england")
+p2_per_capita <- p2_per_capita + ggtitle("Cumulative Daily Confirmed Hospitalisations Per Capita")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p2_per_capita.png",
+  plot = p2_per_capita,
+  width = 10, height = 6, dpi = 320        
+)
+
+# Plot panel 1 with all scenarios
+p <- plot_time_series(p2_sim_data, p1_real_data, "Daily hosps", "england")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p1_all_scenarios.png",
+  plot = p,
+  width = 10, height = 6, dpi = 320        
+)
+#########################################################################
+#Panel 3 - Hospital deaths
+
+p3_sim_data <- simulation_data %>%
+  filter(output_type == "deaths_hosp_inc") %>%
+  filter(version %in% c("Factual", "Counterfactual_principal"))
+p3_real_data <- england_data %>%
+  select(date, region, population, ons_death_hospital)
+colnames(p3_real_data) <- c("time", "region", "population", "value")
+
+p3 <- plot_time_series(p3_sim_data, p3_real_data,
+                       "Daily Hospital Deaths", "england")
+p3 <- p3 + ggtitle("Daily Hospital Deaths")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p3.png",
+  plot = p3,
+  width = 10, height = 6, dpi = 320 
+)
+
+p3_per_capita <- plot_time_series_per_capita(p3_sim_data, p3_real_data,
+                       "Daily Hospital Deaths", "england")
+p3_per_capita <- p3_per_capita + ggtitle("Daily Hospital Deaths Per Capita")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p3_per_capita.png",
+  plot = p3_per_capita,
+  width = 10, height = 6, dpi = 320 
+)
+
+#############################################
+# Panel 4 ICU beds
+p4_sim_data <- simulation_data %>%
+  filter(output_type == "icu") %>%
+  filter(version %in% c("Factual", "Counterfactual_principal"))
+p4_real_data <- england_data %>%
+  select(date, region, population, phe_occupied_mv_beds) #mech_vent_covid) #phe_occupied_mv_beds 
+colnames(p4_real_data) <- c("time", "region", "population", "value")
+
+p4 <- plot_time_series(p4_sim_data, p4_real_data,
+                       "ICU beds", "england")
+p4 <- p4 + ggtitle("Occuppied ICU beds - England")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p4.png",
+  plot = p4,
+  width = 10, height = 6, dpi = 320 
+)
+
+############################################
+# Panel 5 Hospital deaths cumulative
+
+p5_sim_data <- simulation_data %>%
+  filter(output_type == "deaths_hosp_inc")
+p5 <- plot_cumulative(p5_sim_data, "Hospital \nDeaths (thousands)", "england", -20)
+p5 <- p5 + ggtitle("Cumulative Hospital Deaths")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p5.png",
+  plot = p5,
+  width = 10, height = 6, dpi = 320        
+)
+
+p5_per_capita <- plot_cumulative_per_capita(p5_sim_data, "Hospitalisations", "england", -0.0003)
+p5_per_capita <- p5_per_capita + ggtitle("Cumulative Hospital Deaths Per Capita")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p5_per_capita.png",
+  plot = p5_per_capita,
+  width = 10, height = 6, dpi = 320        
+)
+
+# Plot panel 3 with all scenarios
+p <- plot_time_series(p5_sim_data, p3_real_data, "Daily Hospital Deaths", "england")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p3_all_scenarios.png",
+  plot = p,
+  width = 10, height = 6, dpi = 320        
+)
+
+#################################################
+# Panel 6 - IHR
+p6_sim_data <- simulation_data %>%
+  filter(output_type == "ihr") %>%
+  filter(version %in% c("Factual", "Counterfactual_principal"))
+p6_real_data <- data.frame(time = NA, region = NA, population = NA, value = NA)
+
+p6 <- plot_time_series(p6_sim_data, p6_real_data,
+                       "IHR", "england")
+p6 <- p6 + ggtitle("IHR")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p6.png",
+  plot = p6,
+  width = 10, height = 6, dpi = 320 
+)
+
+#################################################
+# Panel 7 - IFR
+p7_sim_data <- simulation_data %>%
+  filter(output_type == "ifr") %>%
+  filter(version %in% c("Factual", "Counterfactual_principal"))
+p7_real_data <- data.frame(time = NA, region = NA, population = NA, value = NA)
+
+p7 <- plot_time_series(p7_sim_data, p7_real_data,
+                       "IFR", "england")
+p7 <- p7 + ggtitle("IFR")
+ggsave(
+  filename = "Figures/individual_panels/Fig2_p7.png",
+  plot = p7,
+  width = 10, height = 6, dpi = 320 
+)
+
+#################################################
+# Put together a final figure
+
+Fig2 <- plot_grid(p1, p2, 
+                  p3 + theme(legend.position = "none"), 
+                  p5 + theme(legend.position = "none"), 
+                  p6 + theme(legend.position = "none"), 
+                  p7 + theme(legend.position = "none"), 
+                  nrow = 3, ncol = 2, 
+                  labels = "AUTO",
+                  #rel_widths = c(1.5, 1), 
+                  align = "v")
+Fig2 <- Fig2 + theme(plot.background = element_rect(fill = "white", colour = NA))
+#final_patch <- p_england | p_map + plot_layout(widths = c(5, 2))
+
+ggsave("Fig2.png", Fig2, width = 16, height = 12, dpi = 320)
+ggsave("Fig2.pdf", Fig2, width = 16, height = 12, dpi = 320)
