ae91d010298abdd5caeba2966705ca59ff8ed85e
Thomas Rawson <thomas.rawson@demography.ox.ac.uk>
2026-03-30 15:32:52 +0100

    Tidy up the run.R

--- run.R
+++ run.R
@@ -22,100 +22,59 @@ orderly_run("02_parameters",
                                                                                   #"baseline_scaled_up" to keep the distribution but just scale it to match population change
             use_draft = "newer")
 
+pop_assumptions <- c("ONS_NHS_region_principal", "ONS_NHS_region_low_migration", "ONS_NHS_region_high_migration")
+pop_years <- c(2047, 2042, 2037, 2032, 2027)
+for(j in pop_assumptions){
+  for(k in pop_years){
+    orderly_run("02_parameters",
+                parameters = list(population_assumptions = j,
+                                  population_year = k,
+                                  vaccine_assumptions = "baseline_scaled_up"),
+                use_draft = "newer")
+  }
+}
+
 ################################################
 # Run simulations
 # This bit can take a while
 regions <- sircovid::regions("england")
 for(r in regions){
-  # orderly_run("03_simulations",
-  #                       parameters = list(population_assumptions = "ONS_NHS_region_principal",
-  #                                         region = region),
-  #                       use_draft = "newer")
-  orderly_run("03_simulations",
-              parameters = list(population_assumptions = "rtm_baseline",
-                                vaccine_assumptions = "baseline",
-                                region = r,
-                                param_iterations = 100),
-              use_draft = "newer")
-  
-  # orderly_run("03_simulations",
-  #             parameters = list(population_assumptions = "ONS_NHS_region_principal",
-  #                               population_year = 2032,
-  #                               vaccine_assumptions = "baseline_scaled_up",
-  #                               region = r,
-  #                               param_iterations = 100),
-  #             use_draft = "newer")
-  
-  # orderly_run("03_simulations",
-  #             parameters = list(population_assumptions = "ONS_NHS_region_low_migration",
-  #                               vaccine_assumptions = "baseline_scaled_up",
-  #                               region = r,
-  #                               param_iterations = 10000),
-  #             use_draft = "newer")
-  # 
-  # orderly_run("03_simulations",
-  #             parameters = list(population_assumptions = "ONS_NHS_region_high_migration",
-  #                               vaccine_assumptions = "baseline_scaled_up",
-  #                               region = r,
-  #                               param_iterations = 10000),
-  #             use_draft = "newer")
-  
+  for(j in pop_assumptions){
+    for(k in pop_years){
+      orderly_run("03_simulations",
+                  parameters = list(population_assumptions = j,
+                                    population_year = k,
+                                    vaccine_assumptions = "baseline_scaled_up",
+                                    region = r,
+                                    param_iterations = 100),
+                  use_draft = "newer")
+    }
+  }
 }
 
 
-
-orderly_develop_start("03_simulations",
-            parameters = list(population_assumptions = "ONS_NHS_region_principal",
-                              vaccine_assumptions = "reallocate_same_number",
-                              region = "london"),
-            use_draft = "newer")
-
-orderly_run("03_simulations",
-                      parameters = list(population_assumptions = "ONS_NHS_region_principal",
-                                        vaccine_assumptions = "reallocate_same_number",
-                                        region = "london",
-                                        param_iterations = 100),
-                      use_draft = "newer")
 ################################################
 # Combine simulations into one dataframe for plotting
+  for(j in pop_assumptions){
+    for(k in pop_years){
+      orderly_run("04_combine_simulations",
+                  parameters = list(population_assumptions = j,
+                                    population_year = k,
+                                    vaccine_assumptions = "baseline_scaled_up",
+                                    param_iterations = 100),
+                  use_draft = "newer")
+    }
+  }
 
 orderly_run("04_combine_simulations",
-                      parameters = list(population_assumptions = "ONS_NHS_region_principal",
-                                        population_year = 2032,
-                                        vaccine_assumptions = "baseline_scaled_up",
+                      parameters = list(population_assumptions = "rtm_baseline",
+                                        population_year = 2019,
+                                        vaccine_assumptions = "baseline",
                                         param_iterations = 100),
                       use_draft = "newer")
 
 
 ##################################################
-#RTM BASELINE CHECK
-orderly_run("02_parameters",
-            parameters = list(population_assumptions = "ONS_NHS_region_low_migration",
-                              vaccine_assumptions = "baseline_scaled_up"),
-            use_draft = "newer")
-# Run simulations
-regions <- sircovid::regions("england")
-for(region in regions){
-  orderly_run("03_simulations",
-                        parameters = list(population_assumptions = "rtm_baseline",
-                                          vaccine_assumptions = "baseline",
-                                          region = region,
-                                          param_iterations = 100),
-                        use_draft = "newer")
-  # orderly_run("03_simulations",
-  #             parameters = list(population_assumptions = "rtm_baseline",
-  #                               region = region,
-  #                               param_iterations = 1000),
-  #             use_draft = "newer")
-}
-
-orderly_run("04_combine_simulations",
-            parameters = list(population_assumptions = "rtm_baseline",
-                              vaccine_assumptions = "baseline",
-                              param_iterations = 100),
-            use_draft = "newer")
-
-#########################
 # Plotting
 # Examine the difference between the population assumptions
 orderly_run("0S1_population_plots",
@@ -123,7 +82,8 @@ orderly_run("0S1_population_plots",
             use_draft = "newer")
 
 orderly_run("0S2_health_outcome_plots",
-                      parameters = list(population_assumptions = "ONS_NHS_region_low_migration",
+                      parameters = list(population_assumptions = "ONS_NHS_region_principal",
+                                        population_year = 2047,
                                         vaccine_assumptions = "baseline_scaled_up",
                                         param_iterations = 100),
                       use_draft = "newer")
@@ -147,3 +107,20 @@ orderly_run("0S3_vaccine_schedule_plots",
                                         vaccine_assumptions = "baseline_scaled_up",
                                         param_iterations = 20),
                       use_draft = "newer")
+
+####################
+# Main paper figures
+orderly_run("Fig1_Population_Change",
+            parameters = list(population_assumptions = "ONS_NHS_region_principal",
+                              population_year = 2047),
+            use_draft = "newer")
+
+orderly_run("Fig2_Health_Outcomes",
+            parameters = list(vaccine_assumptions = "baseline_scaled_up",
+                              population_year = 2047),
+            use_draft = "newer")
+
+orderly_develop_start("Fig3_Transmission_Differences",
+            parameters = list(vaccine_assumptions = "baseline_scaled_up",
+                              population_year = 2047),
+            use_draft = "newer")
