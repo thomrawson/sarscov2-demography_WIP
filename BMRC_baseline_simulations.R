9a68d0a1fdacdf17cc5026fa96b9a12f0af0f2a7
Thomas Rawson <thomas.rawson@demography.ox.ac.uk>
2026-03-31 15:33:17 +0100

    task to do the 7 baseline rtm simulations on BMRC cluster

---
+++ BMRC_baseline_simulations.R
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly = TRUE)
+
+# Get SLURM task ID
+task_id <- as.integer(args[1])
+
+regions <- sircovid:::regions("england")
+
+# Create all combinations
+param_grid <- expand.grid(
+  i = regions,
+  stringsAsFactors = FALSE
+)
+
+# Safety check
+if (task_id > nrow(param_grid)) {
+  stop("Task ID exceeds number of parameter combinations")
+}
+
+# Select this job's parameters
+i <- param_grid$i[task_id]
+
+library(orderly1)
+
+orderly_run(
+  "03_simulations",
+  parameters = list(
+    region = i,
+    population_assumptions = "rtm_baseline",
+    population_year = 2019,
+    vaccine_assumptions = "baseline",
+    param_iterations = 50000
+  ),
+  use_draft = "newer"
+)
