b9522fb09aaca5375c43df8b407bd277231bd883
Thomas Rawson <thomas.rawson@demography.ox.ac.uk>
2026-03-31 15:00:50 +0100

    BMRC 04 task slurm submission

---
+++ BMRC_04_combine_simulations_submission.R
@@ -0,0 +1,39 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly = TRUE)
+
+# Get SLURM task ID
+task_id <- as.integer(args[1])
+
+pop_assumptions <- c(
+  "ONS_NHS_region_principal",
+  "ONS_NHS_region_low_migration",
+  "ONS_NHS_region_high_migration"
+)
+
+pop_years <- c(2047, 2042, 2037, 2032, 2027)
+
+# Create all combinations
+param_grid <- expand.grid(
+  j = pop_assumptions,
+  k = pop_years,
+  stringsAsFactors = FALSE
+)
+
+# Safety check
+if (task_id > nrow(param_grid)) {
+  stop("Task ID exceeds number of parameter combinations")
+}
+
+# Select this job's parameters
+j <- param_grid$j[task_id]
+k <- param_grid$k[task_id]
+
+library(orderly1)
+
+orderly_run("04_combine_simulations",
+            parameters = list(population_assumptions = j,
+                              population_year = k,
+                              vaccine_assumptions = "baseline_scaled_up",
+                              param_iterations = 100),
+            use_draft = "newer")
