bac58b587f6cc3cf1f6556b424acd04ebc554efa
Thomas Rawson <thomas.rawson@demography.ox.ac.uk>
2026-04-13 15:50:22 +0100

    Add 05_combine_vaccinations cluster job

---
+++ BMRC_05_combine_vaccinations_submission.R
@@ -0,0 +1,51 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly = TRUE)
+
+# Get SLURM task ID
+task_id <- as.integer(args[1])
+
+pop_assumptions <- c(
+  "ONS_NHS_region_principal",
+  "ONS_NHS_region_low_migration",
+  "ONS_NHS_region_high_migration"
+)
+
+pop_years <- c(2047, 2042, 2037, 2032, 2027)
+
+vacc_assumptions <- c("baseline",
+                      "baseline_scaled_up",
+                      "baseline_scaled_up_and_reallocated")
+
+# Create all combinations
+param_grid <- expand.grid(
+  i = pop_assumptions,
+  j = pop_years,
+  k = vacc_assumptions,
+  stringsAsFactors = FALSE
+)
+
+#Add in the baseline
+param_grid <- rbind(param_grid, data.frame(i = "rtm_baseline",
+                                           j = 2019,
+                                           k = "baseline"))
+
+# Safety check
+if (task_id > nrow(param_grid)) {
+  stop("Task ID exceeds number of parameter combinations")
+}
+
+# Select this job's parameters
+i <- param_grid$i[task_id]
+j <- param_grid$j[task_id]
+k <- param_grid$k[task_id]
+
+library(orderly1)
+
+orderly_run("05_combine_vaccinations",
+            parameters = list(population_assumptions = i,
+                              population_year = j,
+                              vaccine_assumptions = k,
+                              #param_iterations = 50000),
+                              param_iterations = 200),
+            use_draft = "newer")
