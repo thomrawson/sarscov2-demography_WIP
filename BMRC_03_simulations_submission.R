c3f386382cc641540b32836824b25ab16d9a3472
Thomas Rawson <thomas.rawson@demography.ox.ac.uk>
2026-04-13 15:07:43 +0100

    50,000 iteration cluster

--- BMRC_03_simulations_submission.R
+++ BMRC_03_simulations_submission.R
@@ -48,7 +48,7 @@ orderly_run(
     population_assumptions = i,
     population_year = j,
     vaccine_assumptions = k,
-    param_iterations = 200
+    param_iterations = 50000
   ),
   use_draft = "newer"
 )
--- BMRC_04_combine_simulations_submission.R
+++ BMRC_04_combine_simulations_submission.R
@@ -46,14 +46,12 @@ orderly_run("04_combine_simulations",
             parameters = list(population_assumptions = i,
                               population_year = j,
                               vaccine_assumptions = k,
-                              #param_iterations = 50000),
-                              param_iterations = 200),
+                              param_iterations = 50000),
             use_draft = "newer")
 
 orderly_run("06_parsed_simulations",
             parameters = list(population_assumptions = i,
                               population_year = j,
                               vaccine_assumptions = k,
-                              #param_iterations = 50000),
-                              param_iterations = 200),
+                              param_iterations = 50000),
             use_draft = "newer")
