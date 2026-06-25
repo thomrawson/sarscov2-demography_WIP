02fd48abce000afa74e5a6de3c737552ebddddf0
Thomas Rawson <thomas.rawson@demography.ox.ac.uk>
2026-06-25 12:16:32 +0100

    Add new vaccine scenario

--- run.R
+++ run.R
@@ -12,28 +12,41 @@ orderly_commit(parsed_data_task)
 orderly_run("02_parameters",
             parameters = list(population_assumptions = "ONS_NHS_region_principal", # "ONS_NHS_region_principal" is the baseline ONS regional projections for 2047, 
                                                                                    #  "rtm_baseline" for your bog-standard normal model runs
-                                                                                  # "ONS_NHS_region_low" is the low migration scenario ONS regional projections for 2047, 
-                                                                                  # "ONS_NHS_region_high" is the low migration scenario ONS regional projections for 2047, 
+                                                                                  # "ONS_NHS_region_low_migration" is the low migration scenario ONS regional projections for 2047, 
+                                                                                  # "ONS_NHS_region_high_migration" is the low migration scenario ONS regional projections for 2047, 
                               
-                              population_year = 2032,                             #2019 is the default which should only be used for "rtm_baseline" population_assumptions. Otherwise, 2027,2032,2037,2042,2047
+                              population_year = 2047,                             #2019 is the default which should only be used for "rtm_baseline" population_assumptions. Otherwise, 2027,2032,2037,2042,2047
                               
-                              vaccine_assumptions = "baseline_scaled_up"),    #"reallocate_same_number" keeps exactly the same number of daily vaccines, but redistributes them to different ages according to priority list
+                              vaccine_assumptions = "baseline_scaled_up_and_reallocated"),    #"reallocate_same_number" keeps exactly the same number of daily vaccines, but redistributes them to different ages according to priority list
                                                                                   #"baseline" to use exact real world vaccine data
                                                                                   #"baseline_scaled_up" to keep the distribution but just scale it to match population change
-            use_draft = "newer")
+                                                                                  #"baseline_scaled_up_and_reallocated" to scale the total daily vaccines given by the region's population difference, and then re-allocate
+                                                                                  #"vaccinate_young_earlier" to swap around when vaccines are opened up to 50-54s with 25-29s. 55-59s with 30-34s. 60-64s with 35-39s.
+                                                                                  #"1_month_earlier" is same as baseline_scaled_up_and_reallocated but doses 1 and 2 are shifted forward 30 days
+            use_draft = "newer")  
 
 pop_assumptions <- c("ONS_NHS_region_principal", "ONS_NHS_region_low_migration", "ONS_NHS_region_high_migration")
-pop_years <- c(2047, 2042, 2037, 2032, 2027)
+pop_years <- c(2047, 2037, 2027)
+vacc <- c("baseline", "baseline_scaled_up")
+#pop_years <- c(2047)
 for(j in pop_assumptions){
   for(k in pop_years){
-    orderly_run("02_parameters",
-                parameters = list(population_assumptions = j,
-                                  population_year = k,
-                                  vaccine_assumptions = "baseline_scaled_up"),
-                use_draft = "newer")
+    for(l in vacc){
+      orderly_run("02_parameters",
+                  parameters = list(population_assumptions = j,
+                                    population_year = k,
+                                    vaccine_assumptions = l),
+                  use_draft = "newer")
+    }
+    
   }
 }
 
+orderly_run("02_parameters",
+            parameters = list(population_assumptions = "rtm_baseline",
+                              population_year = 2019,
+                              vaccine_assumptions = "baseline"),
+            use_draft = "newer")
 ################################################
 # Run simulations
 # This bit can take a while
@@ -52,12 +65,27 @@ for(r in regions){
   }
 }
 
+for(r in regions){
+  orderly_run("03_simulations",
+              parameters = list(population_assumptions = "rtm_baseline",
+                                population_year = 2019,
+                                vaccine_assumptions = "baseline",
+                                region = r,
+                                param_iterations = 20),
+              use_draft = "newer")
+}
 
 ################################################
 # Combine simulations into one dataframe for plotting
   for(j in pop_assumptions){
     for(k in pop_years){
       orderly_run("04_combine_simulations",
+                  parameters = list(population_assumptions = j,
+                                    population_year = k,
+                                    vaccine_assumptions = "baseline_scaled_up",
+                                    param_iterations = 100),
+                  use_draft = "newer")
+      orderly_run("06_parsed_simulations",
                   parameters = list(population_assumptions = j,
                                     population_year = k,
                                     vaccine_assumptions = "baseline_scaled_up",
@@ -70,8 +98,14 @@ orderly_run("04_combine_simulations",
                       parameters = list(population_assumptions = "rtm_baseline",
                                         population_year = 2019,
                                         vaccine_assumptions = "baseline",
-                                        param_iterations = 100),
+                                        param_iterations = 20),
                       use_draft = "newer")
+orderly_run("06_parsed_simulations",
+            parameters = list(population_assumptions = "rtm_baseline",
+                              population_year = 2019,
+                              vaccine_assumptions = "baseline",
+                              param_iterations = 20),
+            use_draft = "newer")
 
 
 ##################################################
@@ -84,43 +118,50 @@ orderly_run("0S1_population_plots",
 orderly_run("0S2_health_outcome_plots",
                       parameters = list(population_assumptions = "ONS_NHS_region_principal",
                                         population_year = 2047,
-                                        vaccine_assumptions = "baseline_scaled_up",
-                                        param_iterations = 100),
+                                        vaccine_assumptions = "baseline_scaled_up_and_reallocated",
+                                        param_iterations = 200),
                       use_draft = "newer")
 
 
 orderly_run("05_combine_vaccinations",
             parameters = list(population_assumptions = "ONS_NHS_region_principal",
                               vaccine_assumptions = "baseline_scaled_up",
-                              param_iterations = 20),
-            use_draft = "newer")
-
-
-orderly_develop_start("0S3_vaccine_schedule_plots",
-            parameters = list(population_assumptions = "ONS_NHS_region_principal",
-                              vaccine_assumptions = "reallocate_same_number",
+                              population_year = 2047,
                               param_iterations = 100),
             use_draft = "newer")
 
 orderly_run("0S3_vaccine_schedule_plots",
                       parameters = list(population_assumptions = "ONS_NHS_region_principal",
-                                        vaccine_assumptions = "baseline_scaled_up",
-                                        param_iterations = 20),
+                                        vaccine_assumptions = "baseline",
+                                        population_year = 2047,
+                                        param_iterations = 200),
                       use_draft = "newer")
 
 ####################
 # Main paper figures
 orderly_run("Fig1_Population_Change",
             parameters = list(population_assumptions = "ONS_NHS_region_principal",
+                              vaccine_assumptions = "baseline_scaled_up_and_reallocated",
+                              param_iterations = 50000,
                               population_year = 2047),
             use_draft = "newer")
 
 orderly_run("Fig2_Health_Outcomes",
-            parameters = list(vaccine_assumptions = "baseline_scaled_up",
-                              population_year = 2047),
+            parameters = list(vaccine_assumptions = "baseline_scaled_up_and_reallocated",
+                              param_iterations = 50000),
             use_draft = "newer")
 
-orderly_develop_start("Fig3_Transmission_Differences",
-            parameters = list(vaccine_assumptions = "baseline_scaled_up",
-                              population_year = 2047),
+orderly_run("Fig3_Transmission_Differences",
+            parameters = list(vaccine_assumptions = "baseline_scaled_up_and_reallocated",
+                              population_year = 2047,
+                              param_iterations = 50000),
+            use_draft = "newer")
+
+orderly_run("Fig4_Facetting",
+            parameters = list(param_iterations = 50000),
+            use_draft = "newer")
+
+orderly_run("Fig6_Vaccine_Illustrative",
+            parameters = list(population_assumptions = "ONS_NHS_region_principal",
+                              param_iterations = 200),
             use_draft = "newer")
--- src/02_parameters/R/baseline.R
+++ src/02_parameters/R/baseline.R
@@ -618,6 +618,67 @@ create_baseline <- function(region, date, restart_date,
       booster_daily_doses_value = booster_daily_doses_value
     )
     
+    }else if(vaccine_assumptions == "vaccinate_young_earlier"){
+      #This only makes sense if we're not doing rtm_baseline population
+      stopifnot(population_assumptions != "rtm_baseline")
+      # This scenario assumes that we scale all total daily doses by the total population change in that respective region
+      # eg. if there is 8% more total population in a region, then you give 108 doses instead of 100
+      # This is then reallocated according to rollout strategy
+      # Note, this is different to baseline_reallocated, which actually scales up each daily doses by age by the age-specific population change
+      # This is more reasonable as it's a better metric of staffing and GDP
+      #First do the "scale up" chunk:
+      baseline_pop <- sircovid:::sircovid_population(region)
+      difference_in_pop <- sum(population_SET)/sum(baseline_pop)
+      hold_new_pop <- round(vaccine_schedule_real$doses*difference_in_pop #Add the CHW CHR at the end
+      ) 
+      # And set it
+      vaccine_schedule_real$doses <- hold_new_pop
+      
+      ## And now the reallocating part:
+      #Sum over all ages
+      daily_doses_value <- colSums(vaccine_schedule_real$doses, dims = 1L)
+      #Extract booster doses:
+      booster_daily_doses_value <-daily_doses_value[3,]
+      daily_doses_value <- colSums(daily_doses_value[-3,], dims = 1L)
+      date_start <- vaccine_schedule_real$date
+      
+      vaccine_eligibility <-
+        sircovid::vaccine_eligibility(vaccine_eligibility_min_age)
+      if(population_assumptions == "rtm_baseline"){
+        priority_population <- sircovid::vaccine_priority_population(region = region, 
+                                                                     uptake = vaccine_uptake[,1] * vaccine_eligibility)
+      }else if(population_assumptions %in% c("ONS_NHS_region_principal", "ONS_NHS_region_low_migration", "ONS_NHS_region_high_migration")){
+        priority_population <- 
+          round(sircovid::vaccine_priority_proportion(uptake = vaccine_uptake[,1] * vaccine_eligibility) * c(population_SET, 0, 0))
+      }else{
+        stop("Error: Incompatible vaccine and population assumptions when defining new vaccine schedule.")
+      }
+      # Now re-order the priority_population matrix so that some younger groups get vaccinate before some older groups.
+      #Swap the 50-54s with the 25-29s:
+      priority_population[11,12] <- priority_population[11,9]
+      priority_population[11,9] <- 0
+      priority_population[6,9] <- priority_population[6,12]
+      priority_population[6,12] <- 0
+      #Swap the 55-59s with 30-34s
+      priority_population[12,11] <- priority_population[12,8]
+      priority_population[12,8] <- 0
+      priority_population[7,8] <- priority_population[7,11]
+      priority_population[7,11] <- 0
+      #Swap the 60-64s with 35-39s
+      priority_population[13,11] <- priority_population[13,7]
+      priority_population[13,7] <- 0
+      priority_population[8,7] <- priority_population[8,11]
+      priority_population[8,11] <- 0
+      
+      #change to _real
+      vaccine_schedule_real <- sircovid::vaccine_schedule_future(
+        date_start,
+        daily_doses_value,
+        mean_days_between_doses,
+        priority_population,
+        booster_daily_doses_value = booster_daily_doses_value
+      )
+      
     }else if(vaccine_assumptions == "1_month_earlier"){
       #Here we do the same as scaled_up_and_reallocated, but we start vaccines 1 month earlier
       stopifnot(population_assumptions != "rtm_baseline")
